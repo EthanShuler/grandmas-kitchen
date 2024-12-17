9fa16a983fcae0688df8ffc1ee0590a180bb049e
EthanShuler <13ethanshuler@gmail.com>
2024-12-16 16:52:43 -0800

    fix menu

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="content-center">
+      <h1 className="text-bold text-3xl">About</h1>
+      <p>For Grandma's (and other Roggensack!) Recipes</p>
+    </div>
+  )
+}
--- components/header.tsx
+++ components/header.tsx
@@ -10,22 +10,12 @@ const links = [
   {
     id: 0,
     text: "Home",
-    url: "#"
+    url: "/"
   },
   {
     id: 1,
     text: "About",
-    url: "#"
-  },
-  {
-    id: 2,
-    text: "Contact",
-    url: "#"
-  },
-  {
-    id: 3,
-    text: "Services",
-    url: "#"
+    url: "/about"
   },
 ];
 
@@ -37,15 +27,6 @@ export default async function Navbar() {
   } = await supabase.auth.getUser();
 
   return (
-    // <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-    //   <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-    //     <div className="flex gap-5 items-center font-semibold">
-    //       <Link href={"/"}>Grandma's Kitchen</Link>
-    //     </div>
-    //     <HeaderAuth />
-    //   </div>
-    // </nav>
-
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
       <Sheet>
         <SheetTrigger asChild>
@@ -90,12 +71,6 @@ export default async function Navbar() {
                     Sign Up
                   </Link>
                 </Button>
-                {/* <Link href="/sign-in" className="flex w-full items-center py-2 text-lg font-semibold" prefetch={false}>
-                  Sign in
-                </Link>
-                <Link href="/sign-up" className="flex w-full items-center py-2 text-lg font-semibold" prefetch={false}>
-                  Sign Up
-                </Link> */}
               </>
             )}
           </div>
@@ -152,29 +127,6 @@ export default async function Navbar() {
                 Sign Up
               </Link>
             </Button>
-
-            {/* <Link
-              href="/sign-in"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm
-            font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none
-            disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950
-            dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50
-            dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Sign In
-            </Link>
-            <Link
-              href="sign-up"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm
-            font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none
-            disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950
-            dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50
-            dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Sign up
-            </Link> */}
           </>
         )}
       </nav>
